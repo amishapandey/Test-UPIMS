3ac26e7be9a76dc0fb2586db5eb1d622ac3c9dd5
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-22 12:37:53 +0530

    Update s1_q3.java

--- s1_q3.java
+++ s1_q3.java
@@ -1,31 +1,23 @@
-import java.util.*;
+mport java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
 	    int arr[]=new int[n];
-	    int arr1[]=new int[n];
 	    int i;
 	    for(i=0;i<n;i++){
 	    arr[i]=sc.nextInt();
 	    }
 	    for(i=0;i<n;i++)
 	    {
-	        if(i!=arr[i])
-	        continue;
-	        else
-	        {
-	        for(i=0;i<n;i++)
-	        {
-	            arr1[i]=arr[i];
-	        }
-	        }
-	        
-	    }Arrays.sort(arr1);
-	    for(i=0;i<n;i++){
-		System.out.print(arr1[i]);
-		System.out.print(' ');
+	       if(i==arr[i]){
+	       System.out.print(arr[i]);
+		  System.out.print(' ');
+	           
+	       }
+	       else 
+	       continue;
 	    }
 		
 	}
