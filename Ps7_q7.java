9dc246c507f264c1d2362b543905816b88dc963b
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-06-07 20:56:33 +0530

    Create Ps7_q7.java

---
+++ Ps7_q7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+		int i,fact=1;
+		for(i=1;i<=n;i++){
+		fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+}
