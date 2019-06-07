73aa661799fcb3ea6c0382c5cc9ceb76fc248aa0
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-06-07 18:04:36 +0530

    Update and rename PS7_q9,java to PS7_q9.java

--- PS7_q9.java
+++ PS7_q9.java
@@ -3,7 +3,7 @@ public class Main
 {
 	public static void main (String[] args) 
 	{
-		Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
 		int k=sc.nextInt();
 		int arr[]=new int[n];
