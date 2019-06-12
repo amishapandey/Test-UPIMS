0c40ad521ac68dda8660ba0124093ac0a1827c36
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-06-13 00:39:44 +0530

    Create HS13_q4.c

---
+++ HS13_q4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   int i,j;
+   for(i=1;i<=n;i++){
+       for(j=i;j<=n;j++){
+       printf("1");
+       printf(" ");
+       }printf("\n");
+   }
+    return 0;
+}
